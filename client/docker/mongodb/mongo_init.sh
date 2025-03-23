733b1efb9fb6d877e1b5ab93c94f4e9ca5bf37ac
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-23 23:01:20 +0100

    sat the Dockerfile to call the init script (so not the compose anymore). changed the init script to only be called ones. made the compose file build our Dockerfile, which uses the base mongodb image.

--- client/docker/mongodb/mongo_init.sh
+++ client/docker/mongodb/mongo_init.sh
@@ -1,44 +1,69 @@
 # this script should only be runned the first time the docker compose gets run
-# TODO - make sure it only run one time
 
-# this script will be setup all our tables, users, privileges and raw data
+# this one line is just to make it easier to find in the debug log
+echo "MongoDB docker 'init script' have been started **************
+**************************************************************
+***************************************************************
+****************************************************************"
 
-# create db
-mongo --eval 'use shop;'
+# if the docker volume is initialized, then skip the rest of the file
+if [ -f /data/db/initialized ]; then
+    echo "MongoDB is already initialized. Therefore the init script will be skipped"
+    exit 0
+fi
 
-# create tables / collections
-mongo --eval 'db.createCollection("product");'
-mongo --eval 'db.createCollection("order");'
-mongo --eval 'db.createCollection("user");'
+# this script will be setting up all our tables, users, privileges and raw data
 
-# add roles
+# Wait for MongoDB to start
+echo "Waiting for MongoDB to start..."
+sleep 10
 
-# ...... more code goes here
+# start the program
+mongosh -u "$MON_USERNAME" -p "$MON_PASSWORD" --authenticationDatabase 'admin'
 
-# add dev user
-mongo --eval "db.createUser({
-    user: \"$MON_D1_USE\", 
-    pwd: \"$MON_D1_PASS\", 
-    roles:[{role: \"root\", db: \"shop\"}] 
-});"
+# # create db
+use shop
+
+# # create tables / collections 
+# # these are not our final tables, just for testing right now
+db.createCollection("product"); # THIS WORKS
+sleep 1
+db.createCollection("order"); # everything after "product" does not get run for some reason # TODO - work on this
+sleep 1
+db.createCollection("user");
+sleep 1
+
+# # add roles
+
+# # ...... more code goes here
+
+# # add dev user
+db.createUser({
+    user: "$MON_D1_USE", 
+    pwd: "$MON_D1_PASS", 
+    roles: [{role: "dbOwner", db: "shop"}] 
+});
 
 # add user1
-mongo --eval "db.createUser({
+db.createUser({
     user: \"$MON_U1_USE\", 
     pwd: \"$MON_U1_PASS\", 
     roles:[{}] 
-});"
+});
 
 # add user2
-mongo --eval "db.createUser({
+db.createUser({
     user: \"$MON_U2_USE\", 
     pwd: \"$MON_U2_PASS\", 
     roles:[{}] 
-});"
+});
 
 # add user3
-mongo --eval "db.createUser({
+db.createUser({
     user: \"$MON_U3_USE\", 
     pwd: \"$MON_U3_PASS\", 
     roles:[{}] 
-});"
+});
+
+# keep the container alive
+wait $! # TODO - check if this line IS needed to not kill the container
