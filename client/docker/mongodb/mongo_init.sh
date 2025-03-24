75b20665072db8bde30fc6932717864c6c2d5816
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-24 14:47:59 +0100

    implemented a working init script (it runs by it self in that folder).

--- client/docker/mongodb/mongo_init.sh
+++ client/docker/mongodb/mongo_init.sh
@@ -6,6 +6,9 @@ echo "MongoDB docker 'init script' have been started **************
 ***************************************************************
 ****************************************************************"
 
+# load data from our .env file
+source .env
+
 # if the docker volume is initialized, then skip the rest of the file
 if [ -f /data/db/initialized ]; then
     echo "MongoDB is already initialized. Therefore the init script will be skipped"
@@ -19,51 +22,114 @@ echo "Waiting for MongoDB to start..."
 sleep 10
 
 # start the program
-mongosh -u "$MON_USERNAME" -p "$MON_PASSWORD" --authenticationDatabase 'admin'
 
-# # create db
-use shop
+mongosh --host "localhost" --u "$MON_USERNAME" --p "$MON_PASSWORD" --authenticationDatabase "admin" <<EOF
+// all the mongosh commands
 
-# # create tables / collections 
-# # these are not our final tables, just for testing right now
-db.createCollection("product"); # THIS WORKS
-sleep 1
-db.createCollection("order"); # everything after "product" does not get run for some reason # TODO - work on this
-sleep 1
-db.createCollection("user");
-sleep 1
+// switch or create db
+use shop;
 
-# # add roles
+// create tables
+db.createCollection('product');
+db.createCollection('order');
+db.createCollection('user');
 
-# # ...... more code goes here
+// add to tables (from data folder - json files ?)
 
-# # add dev user
+// add roles
+db.createRole({
+  role: "shop_manager",
+  privileges: [
+    {
+      resource: { db: "shop", collection: "product" },
+      actions: ["find", "insert"]
+    },
+    {
+      resource: { db: "shop", collection: "order" },
+      actions: ["find", "insert", "remove"]
+    },
+    {
+      resource: { db: "shop", collection: "user" },
+      actions: ["find", "insert"]
+    },
+    {
+      resource: { db: "shop", collection: "" },
+      actions: ["listCollections"]
+    }
+  ],
+  roles: []
+})
+
+db.createRole({
+  role: "shop_worker",
+  privileges: [
+    {
+      resource: { db: "shop", collection: "product" },
+      actions: ["find", "insert"]
+    },
+    {
+      resource: { db: "shop", collection: "order" },
+      actions: ["find"]
+    },
+    {
+      resource: { db: "shop", collection: "" },
+      actions: ["listCollections"]
+    }
+  ],
+  roles: []
+})
+
+db.createRole({
+  role: "shop_driver",
+  privileges: [
+    {
+      resource: { db: "shop", collection: "product" },
+      actions: ["find"]
+    },
+    {
+      resource: { db: "shop", collection: "order" },
+      actions: ["find"]
+    },
+    {
+      resource: { db: "shop", collection: "" },
+      actions: ["listCollections"]
+    }
+  ],
+  roles: []
+})
+
+// add dev user
 db.createUser({
     user: "$MON_D1_USE", 
     pwd: "$MON_D1_PASS", 
     roles: [{role: "dbOwner", db: "shop"}] 
 });
 
-# add user1
+// add user1
 db.createUser({
-    user: \"$MON_U1_USE\", 
-    pwd: \"$MON_U1_PASS\", 
-    roles:[{}] 
+    user: "$MON_U1_USE",
+    pwd: "$MON_U1_PASS", 
+    roles: [{role: "shop_manager", db: "shop"}] 
 });
 
-# add user2
+// add user2
 db.createUser({
-    user: \"$MON_U2_USE\", 
-    pwd: \"$MON_U2_PASS\", 
-    roles:[{}] 
+    user: "$MON_U2_USE", 
+    pwd: "$MON_U2_PASS", 
+    roles: [{role: "shop_worker", db: "shop"}] 
 });
 
-# add user3
+// add user3
 db.createUser({
-    user: \"$MON_U3_USE\", 
-    pwd: \"$MON_U3_PASS\", 
-    roles:[{}] 
+    user: "$MON_U3_USE", 
+    pwd: "$MON_U3_PASS", 
+    roles: [{role: "shop_driver", db: "shop"}] 
 });
+
+// the end
+print("MongoDB initialization script ran successfully!");
+
+EOF
 
 # keep the container alive
-wait $! # TODO - check if this line IS needed to not kill the container
+# wait  # TODO - check if this line IS needed to not kill the container
