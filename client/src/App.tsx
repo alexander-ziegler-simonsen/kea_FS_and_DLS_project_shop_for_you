c8fa6dc99a1690d9d5fff5f2468a17303bef7767
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-30 23:29:17 +0200

    client - front page grid setup and color switch button implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,49 @@
-import { Box, Grid, GridItem, Text } from "@chakra-ui/react"
+import { Box, Button, Grid, GridItem, HStack, Text, VStack } from "@chakra-ui/react"
+import { useColorMode } from "./components/ui/color-mode"
 
 function App() {
+
+  // https://chakra-ui.com/docs/components/concepts/color-mode#usecolormode
+  const { toggleColorMode } = useColorMode()
+
   return (
     <Grid templateAreas={{
       base: `"header" "main"`,
-      lg: `"header header" "aside main`
+      lg: `"header header" "aside main"`
     }}
     templateColumns={{base: "1fr", lg: "200px 1fr" }}>
-      <GridItem pl="2" area={"header"}>Header</GridItem>
-      <GridItem pl="2" area={"aside"}>left side here</GridItem>
-      <GridItem pl="2" area={"main"}>
-        <Box paddingLeft={2}>
-        <Text>big text</Text>
+      <GridItem bg={"orange.300"} area={"header"} p={2}>
+
+        <HStack gap={"5"}>
+          <Text fontWeight={"bold"} fontSize={"2xl"}>Shop for You</Text>
+        <Button variant={"outline"} onClick={toggleColorMode}>
+          toggle color
+        </Button>
+
+        </HStack>
+
         
+      </GridItem>
+
+      <GridItem p={2} bg={"orange.100"} area={"aside"}>left side here
+      <Text>big text</Text>
+        <Text>item</Text>
           <ul>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
           </ul>
           <h1>big new text</h1>
           <ul>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
+            <li><Text>item</Text></li>
           </ul>
-        </Box>
+      </GridItem>
+      <GridItem p={2} bg={"orange.200"} area={"main"}>
+        main box here
       </GridItem>
     </Grid>
   )
