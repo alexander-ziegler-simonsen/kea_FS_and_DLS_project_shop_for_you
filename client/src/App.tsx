011649daf012a9f95679c8aa33249bc253b776cb
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-30 22:58:30 +0200

    client - add provider to main and changed the vite config, so it works with chakra

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react"
+
 function App() {
   return (
-    <>
-      <div>
-        <p>
-          This is just our first commit.
-        </p>
-      </div>
-    </>
+    <Grid templateAreas={{
+      base: `"header" "main"`,
+      lg: `"header header" "aside main`
+    }}
+    templateColumns={{base: "1fr", lg: "200px 1fr" }}>
+      <GridItem pl="2" area={"header"}>Header</GridItem>
+      <GridItem pl="2" area={"aside"}>left side here</GridItem>
+      <GridItem pl="2" area={"main"}>
+        <Box paddingLeft={2}>
+        <Text>big text</Text>
+        
+          <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+          </ul>
+          <h1>big new text</h1>
+          <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+          </ul>
+        </Box>
+      </GridItem>
+    </Grid>
   )
 }
 
