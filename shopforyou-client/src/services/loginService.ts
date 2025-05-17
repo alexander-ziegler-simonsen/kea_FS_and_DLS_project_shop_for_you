48247bcc91ae6679a93a9c5e4cc0a93376481025
Mikkel sandell <mikkelsandell@gmail.com>
2025-05-17 15:43:15 +0200

    made so login and regiser uses user-api-client

--- shopforyou-client/src/services/loginService.ts
+++ shopforyou-client/src/services/loginService.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
+import UserApiClient from "./user-api-client";
 
-const API_URL = "http://localhost:3006/api/users/login";
+const API_URL = "/api/users/login";
+
+const userApiClient = new UserApiClient(API_URL);
 
 export const loginUser = async (credentials: { identifier: string; password: string }) => {
   try {
-    const response = await axios.post(API_URL, credentials);
-    return response.data; // Return the JWT token
+    return await userApiClient.post(credentials);
   } catch (error: any) {
     throw new Error(error.response?.data?.error || "Login failed");
   }
